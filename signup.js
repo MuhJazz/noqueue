76ccf2a102b6bfcc2c98d10f3e283f4c647076c9
Bobby Williams <bobbywilliams@apps.ipb.ac.id>
2021-05-25 20:59:06 +0700

    update signup.js

--- signup.js
+++ signup.js
@@ -1,26 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   const signupUserForm = document.querySelector("#form-daftar-user");
-  const signupPemilikiRestoForm = document.querySelector(
-    "#form-daftar-pemilik-resto"
-  );
+  const signupPemilikiRestoForm = document.querySelector("#form-daftar-pemilik-resto");
   const signupRestoForm = document.querySelector("#form-daftar-resto");
 
-  document
-    .querySelector("#linkFormPemilikResto")
-    .addEventListener("click", (e) => {
-      e.preventDefault();
-      signupUserForm.classList.add("form-daftar-pemilik-resto-hidden");
-      signupPemilikiRestoForm.classList.remove(
-        "form-daftar-pemilik-resto-hidden"
-      );
-    });
-  document
-    .querySelector("#button-lanjut-daftar-resto")
-    .addEventListener("click", (e) => {
-      e.preventDefault();
-      signupPemilikiRestoForm.classList.add("form-daftar-resto-hidden");
-      signupRestoForm.classList.remove("form-daftar-resto-hidden");
-    });
+  document.querySelector("#linkFormPemilikResto").addEventListener("click", (e) => {
+    e.preventDefault();
+    signupUserForm.classList.add("form-daftar-pemilik-resto-hidden");
+    signupPemilikiRestoForm.classList.remove("form-daftar-pemilik-resto-hidden");
+  });
+  document.querySelector("#button-lanjut-daftar-resto").addEventListener("click", (e) => {
+    e.preventDefault();
+    signupPemilikiRestoForm.classList.add("form-daftar-resto-hidden");
+    signupRestoForm.classList.remove("form-daftar-resto-hidden");
+  });
 });
 
 document.getElementById("daftar-user-button").onclick = function (e) {
@@ -30,5 +22,5 @@ document.getElementById("daftar-user-button").onclick = function (e) {
 
 document.getElementById("daftar-resto-button").onclick = function (e) {
   e.preventDefault();
-  location.href = "./homepage-profile.html";
+  location.href = "./homepage-resto.html";
 };
